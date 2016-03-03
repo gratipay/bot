4e36d4739d74769c902419c19860b36684718ebf
Chad Whitacre <chad@zetaweb.com>
2016-03-03 11:38:49 -0500

    Make update conditional on branch status

--- update.sh
+++ update.sh
@@ -1,6 +1,26 @@
 #!/usr/bin/env bash
 set -e
+
 cd /root/bot
+
+# http://stackoverflow.com/a/3278427
+LOCAL=$(git rev-parse @)
+REMOTE=$(git rev-parse @{u})
+BASE=$(git merge-base @ @{u})
+
+if [ $LOCAL = $REMOTE ]; then
+    echo "Up-to-date"
+    exit 0
+elif [ $LOCAL = $BASE ]; then
+    echo "Updating ..."
+elif [ $REMOTE = $BASE ]; then
+    echo "WTF? Need to push."
+    exit 1
+else
+    echo "WTF? Diverged."
+    exit 1
+fi
+
 git pull
 env/bin/pip install --upgrade -r requirements.txt
 crontab crontab
